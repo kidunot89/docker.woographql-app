b1459b6c6e7d9445d56a2f19e1090a2c43c1b29a
Geoff Taylor <geoffrey.taylor@outlook.com>
2019-07-25 13:42:25 -0400

    Build script updated.

--- build.sh
+++ build.sh
@@ -2,25 +2,35 @@
 
 # Image variants
 declare -a variants=(
-    '7.3;5.2.2'
-    '7.2;5.2.2'
-    '7.1;5.2.2'
-    '7.0;5.2.2'
-    '5.6;4.9'
-    '5.6;5.0.2'
+    '5.2;7.3'
+    '5.2;7.2'
+    '5.2;7.1'
+    '5.1;7.3'
+    '5.1;7.2'
+    '5.1;7.1'
+    '5.0;7.3'
+    '5.0;7.2'
+    '5.0;7.1'
+    '5.0;7.0'
+    '5.0;5.6'
+    '4.9;7.2'
+    '4.9;7.1'
+    '4.9;7.0'
+    '4.9;5.6'
 )
 
 for i in "${variants[@]}"; do
     IFS=';' read -ra variant <<< "$i"
 
     # Build tag
-    tag="wp${variant[1]}-php${variant[0]}"
+    tag="wp${variant[0]}-php${variant[1]}"
 
     # Build image
     echo "Building kidunot89/woographql-app:${tag}"
     docker build -t kidunot89/woographql-app:${tag} \
-        --build-arg WP_VERSION=${variant[1]} \
-        --build-arg PHP_VERSION=${variant[0]} \
+        --build-arg WP_VERSION=${variant[0]} \
+        --build-arg PHP_VERSION=${variant[1]} \
         .
+
 done
 
